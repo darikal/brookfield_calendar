90ebe42a00b9b12385503797f002ca77033950ea
Alex <alex@valettaservices.com>
2026-01-21 19:13:08 -0500

    does this work?

---
+++ Testlog.js
@@ -0,0 +1,11 @@
+fetch("http://localhost:3000/api/login", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    username: "admin",
+    password: "FebbraioValettaAlex"
+  })
+});
+
+
+
