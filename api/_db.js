e1a96790a15953cca84e650417fd04f1b2220f2b
Alex <alex@valettaservices.com>
2026-01-28 17:05:29 -0500

    addEvent.js

--- api/_db.js
+++ api/_db.js
@@ -1,22 +1,20 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-  throw new Error("Missing MONGODB_URI environment variable");
-}
+if (!uri) throw new Error("MONGODB_URI is missing");
 
 let client;
 let clientPromise;
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, {
-    maxPoolSize: 10
-  });
-
+  client = new MongoClient(uri);
   global._mongoClientPromise = client.connect();
 }
-
 clientPromise = global._mongoClientPromise;
 
 export default clientPromise;
+
+export async function getDB() {
+  const client = await clientPromise;
+  return client.db("calendar");
+}
