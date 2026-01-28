164e7a20f76f817050f4b6c040f81d59bd7fc2e2
Alex <alex@valettaservices.com>
2026-01-28 16:37:09 -0500

    _db.js

--- api/_db.js
+++ api/_db.js
@@ -1,16 +1,27 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error("Missing MONGODB_URI in environment variables");
+
+if (!uri) {
+  throw new Error("Missing MONGODB_URI environment variable");
+}
 
 let client;
 let clientPromise;
 
-if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, { maxPoolSize: 10 });
-  global._mongoClientPromise = client.connect();
+export async function getDB() {
+  try {
+    if (!clientPromise) {
+      client = new MongoClient(uri, {
+        maxPoolSize: 5
+      });
+      clientPromise = client.connect();
+    }
+
+    const connectedClient = await clientPromise;
+    return connectedClient.db();
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    throw err;
+  }
 }
-
-clientPromise = global._mongoClientPromise;
-
-export default clientPromise;
